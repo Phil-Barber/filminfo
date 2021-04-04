6428b55a4335b2a9ebd5e249a07a27b676d8caf1
Phil Barber <phil.barber93@gmail.com>
2021-04-04 16:08:00 +0100

    recur through pages

--- src/select.rs
+++ src/select.rs
@@ -3,16 +3,25 @@ use std::option::Option;
 use dialoguer::Select;
 use crate::search::SearchResult;
 
-fn page<'a>(iter: &mut impl Iterator<Item = &'a SearchResult>, page_size: i8) -> Option<usize> {
+fn page<'a>(
+    iter: &mut impl Iterator<Item = &'a SearchResult>, 
+    page_size: usize,
+) -> Option<usize> {
     let mut selector = Select::new();
-    for _ in 0..page_size {
+    for i in 0..page_size {
         let next = iter.next();
         if !next.is_none() {
             selector.item(next.unwrap());
+        } else if i == 0 {
+            panic!("No more options")
         }
     }
     selector.item("Show more");
-    selector.interact().ok()
+    let chosen = selector.interact().ok()?;
+    if chosen == page_size {
+        return Some(page_size + page(iter, page_size)?);
+    }
+    Some(chosen)
 }
 
 pub fn get_selected_item<'a>(
